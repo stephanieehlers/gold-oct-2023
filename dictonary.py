92d771ae9af164b2d86ecdf6719e349bded02256
Stephanie Ehlers <steph@ehlers.io>
2023-10-17 01:21:59 +0000

    tuples

--- dictonary.py
+++ dictonary.py
@@ -1,3 +1,8 @@
-"""
-Your module description
-"""
+# you can use a dictonary to find something out. 
+
+print(emails['Mark Steel'])
+
+
+print(spanish_animals['bird'])
+
+# dictonaries use curly brackets.
--- tupleslists.py
+++ tupleslists.py
@@ -1,3 +1,13 @@
 """
-Your module description
+tuples as lists and lists as tuples
 """
+
+city_1 = ('London', 'UK', 8.98)
+city_2 = ('Canberra', 'Australia', 0.4)
+city_3 = ('Algiers', 'Algieria', 3.9)
+
+for capital in capitals:
+    print()
+    
+    # if a tuple has something in it like a list then you can still modify it. 
+    
